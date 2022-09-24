2e72e33167af9c33a711f5d6763d3fe5e662e6ec
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2022-09-24 10:05:57 +0200

    Update cv1.sql

--- cv1.sql
+++ cv1.sql
@@ -31,5 +31,29 @@ SELECT count(length) FROM film
 SELECT count(DISTINCT special_features) FROM film
 --vypište minimální, maximální, průměrnou a celkovou délku všech filmů
 SELECT MIN(length) AS minimální, MAX(length) AS maximální, AVG(length) as průměrná, SUM(length) as celková FROM film
---vypište názvy všech filmů 
-SELECT title, name FROM film JOIN language ON film.language_id = language.language_id;
+--vypište názvy všech filmů dle jejich jazyku
+SELECT film.title, language.name 
+FROM film 
+JOIN language ON film.language_id = language.language_id;
+--vypište jména, přijímení, adresy a města všech zákazníků
+SELECT customer.first_name, customer.last_name, address.address, city.city 
+FROM customer 
+JOIN address 
+ON customer.address_id = address.address_id 
+JOIN city 
+ON address.city_id = city.city_id;
+--vypište jména filmů, které půjčovna vlastní alespoň v jedné kopii
+SELECT DISTINCT film.title
+FROM film 
+JOIN inventory 
+ON film.film_id = inventory.film_id;
+--vypište všechny filmy a půjčovny, ve kterých jsou
+SELECT DISTINCT film.title, inventory.store_id
+FROM film 
+LEFT JOIN inventory 
+ON film.film_id = inventory.film_id;
+--vypište ID a částky všech plateb a u každé platby uveďte datum výpujčky
+SELECT payment.payment_id, payment.amount, rental.rental_date 
+FROM rental 
+RIGHT JOIN payment 
+ON rental.rental_id = payment.rental_id;
